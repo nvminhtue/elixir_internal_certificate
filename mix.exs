d0bc7fd4a633c267be27ea92ca19169677e1e676
Tue Nguyen <tuenguyen@Tues-MacBook-Pro.local>
2022-06-06 16:58:58 +0700

    [1] Fix could not write coverage report

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ElixirInternalCertificate.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      elixirc_options: [debug_info: Mix.env() == :dev],
+      elixirc_options: [debug_info: true],
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
