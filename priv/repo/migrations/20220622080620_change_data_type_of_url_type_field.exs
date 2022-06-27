433179a77cdbe3c30a1f047591fde97b77b49439
nvminhtue <nvminhtue@gmail.com>
2022-06-27 17:19:12 +0700

    [#10][#11][#12] Change migration of changing column type's datatype

--- priv/repo/migrations/20220622080620_change_data_type_of_url_type_field.exs
+++ priv/repo/migrations/20220622080620_change_data_type_of_url_type_field.exs
@@ -6,4 +6,10 @@ defmodule ElixirInternalCertificate.Repo.Migrations.ChangeDataTypeOfUrlTypeField
       modify :type, :string, nullable: false, default: "non_ad_word"
     end
   end
+
+  def down do
+    execute """
+      alter table url_results alter column type type integer using (type::integer);
+    """
+  end
 end
