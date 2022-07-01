88c757abcdc15bc77436bc06ea74345eba8aa54e
nvminhtue <nvminhtue@gmail.com>
2022-07-01 16:14:58 +0700

    [#13] Implement UI of sraping keywords list

--- test/elixir_internal_certificate_web/controllers/user_search_controller_test.exs
+++ test/elixir_internal_certificate_web/controllers/user_search_controller_test.exs
@@ -27,6 +27,34 @@ defmodule ElixirInternalCertificateWeb.UserSearchControllerTest do
     end
   end
 
+  describe "GET /keywords/:id" do
+    test "when logged in user click on detail button, returns keyword result", %{conn: conn} do
+      user = insert(:user)
+      user_search = insert(:user_search, id: 1, user: user)
+      insert(:search_result, user_search: user_search)
+
+      conn =
+        conn
+        |> log_in_user(user)
+        |> get("/keywords/#{user_search.id}")
+
+      assert html_response(conn, 200) =~ "Statistics"
+    end
+
+    test "when unauthenticated, returns login page", %{conn: conn} do
+      response =
+        conn
+        |> bypass_through(ElixirInternalCertificateWeb.Router, :browser)
+        |> get("/")
+        |> get("/users/log_in")
+        |> html_response(200)
+
+      assert response =~ "Login"
+    end
+
+    # Non existed records will be handled later
+  end
+
   describe "POST /upload" do
     test "when uploading valid file with logged in user, returns home page", %{conn: conn} do
       file = upload_dummy_file("valid.csv")
