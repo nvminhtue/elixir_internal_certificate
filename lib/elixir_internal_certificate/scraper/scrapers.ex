8d25a4a37d5d200601796bf5b31afb178c4c3e8c
nvminhtue <nvminhtue@gmail.com>
2022-09-08 20:38:16 +0700

    [#14] Implement backend of showing scraping keywords

--- lib/elixir_internal_certificate/scraper/scrapers.ex
+++ lib/elixir_internal_certificate/scraper/scrapers.ex
@@ -20,6 +20,19 @@ defmodule ElixirInternalCertificate.Scraper.Scrapers do
     keyword_count
   end
 
+  def get_user_search(id) when is_integer(id) or is_binary(id) do
+    UserSearch
+    |> Repo.get(id)
+    |> Repo.preload(:search_results)
+  end
+
+  def get_user_searches(user_id, page \\ @default_page, page_size \\ @default_page_size) when is_integer(user_id) do
+    UserSearch
+    |> where([u], u.user_id == ^user_id)
+    |> order_by(asc: :id)
+    |> Repo.paginate(page: page, page_size: page_size)
+  end
+
   def update_user_search_status(user_search, status) do
     user_search
     |> UserSearch.status_changeset(status)
