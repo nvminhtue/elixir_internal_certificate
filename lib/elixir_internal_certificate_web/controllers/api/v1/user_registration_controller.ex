fbc2ced7ecddd38be8c84a18f6c6ca0f93a46f99
nvminhtue <nvminhtue@gmail.com>
2022-11-08 15:26:41 +0700

    [gh-17] Fix redundant code on registrating

--- lib/elixir_internal_certificate_web/controllers/api/v1/user_registration_controller.ex
+++ lib/elixir_internal_certificate_web/controllers/api/v1/user_registration_controller.ex
@@ -2,16 +2,10 @@ defmodule ElixirInternalCertificateWeb.Api.V1.UserRegistrationController do
   use ElixirInternalCertificateWeb, :controller
 
   alias ElixirInternalCertificate.Account.Accounts
-  alias ElixirInternalCertificate.Account.Schemas.User
 
   def create(conn, params) do
-    with %{changes: validated_params} = _changeset <-
-           Accounts.change_user_registration(%User{}, params),
-         {:ok, user} <- Accounts.register_user(validated_params) do
+    with {:ok, user} <- Accounts.register_user(params) do
       render(conn, "show.json", %{data: user})
-    else
-      error ->
-        error
     end
   end
 end
